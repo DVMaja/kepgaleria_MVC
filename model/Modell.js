54bbdd9e03442240fe66ff304f26f35964a86994
DVMaja <majadreilinger@gmail.com>
2023-09-26 10:58:36 +0200

    jobb() és bal() kész ezek fognak léptetni

--- model/Modell.js
+++ model/Modell.js
@@ -2,12 +2,36 @@ import { KEPLISTA } from "./adat.js";
 
 class Modell {
     #lista = []
+    #id;
     constructor() {
+        //ez jellemzi a program aktuális állapotát. 
         this.#lista = KEPLISTA;
+        this.#id = 0;
 
     }
     getList() {
         return this.#lista;
     }
+
+    getId() {
+        return this.#id;
+    }
+
+    jobb() {
+        //Növeli az ID értékét
+        this.#id++;
+        if (this.#id >= this.#lista.length) {
+            this.#id = 0;
+        }
+    }
+
+    bal() {
+        //Növeli az ID értékét
+        this.#id--;
+        if (this.#id < 0) {
+            this.#id = this.#lista.length - 1;
+        }
+    }
+
 }
 export default Modell;
