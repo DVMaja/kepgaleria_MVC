ba98382038ce1f2fd8fbdb5c87cc0a77e0d56db7
DVMaja <majadreilinger@gmail.com>
2023-09-26 11:15:50 +0200

    példányosíttottuk a NagyKepView-t

--- controller/Controller.js
+++ controller/Controller.js
@@ -1,6 +1,14 @@
+import Modell from "../model/Modell.js";
+import NagyKepView from "../view/NagyKep.js";
+
 class Controller {
     constructor() {
+        //Példányosítjuk a modellt
+        const MODELL = new Modell();
 
+        //példányosítjuk a viewt
+        const NAGYKEPVIEW = new NagyKepView(MODELL.getAktualisKep(), $(".nagykep"));
+        
     }
 }
 export default Controller;
