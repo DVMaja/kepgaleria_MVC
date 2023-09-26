e2e2b980d63bfbd0b7cf6d9c9059b97db14cea4a
DVMaja <majadreilinger@gmail.com>
2023-09-26 11:14:18 +0200

    nyagykép html elem  létrehozva

--- view/NagyKep.js
+++ view/NagyKep.js
@@ -1,4 +1,26 @@
-class NagyKep {
-    constructor() { }
+class NagyKepView {
+    #kep; //szöveges típus, de lehetne objektum is
+
+    constructor(kep, SzuloElem) { 
+        //MIt akarunk megjeleníteni és hova
+        this.#kep = kep;
+        this.SzuloElem = SzuloElem;
+
+        //HTML tag létrehozása
+        this.#htmlOsszerak();
+
+    }
+
+    #htmlOsszerak(){
+        let txt = "";
+        txt = `<div class="card">
+        <div class="card-header">Főkép</div>
+        <div class="card-body">
+        <img src="${this.#kep}" class="img-thumbnail" alt="Cinque Terre"></div>
+        <div class="card-footer">Leírás</div>
+      </div>`;
+
+      this.SzuloElem.html(txt);
+    }
 }
-export default NagyKep;
+export default NagyKepView;
