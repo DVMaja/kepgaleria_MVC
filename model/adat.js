c733e9454bbafa0e7198ac1767b8b7d8c5a4f2fe
DVMaja <majadreilinger@gmail.com>
2023-10-03 08:47:20 +0200

    adatok.js  leírás és cím

--- model/adat.js
+++ model/adat.js
@@ -1,9 +1,9 @@
 export const KEPLISTA = [{
-    eleres: "kepek/virag_1.jpeg", cim: "Virág", leiras: ""
+    eleres: "kepek/virag_1.jpeg", cim: "1Virág", leiras: "1 kép leírása"
 }, {
-    eleres: "kepek/virag_2.jpeg", cim: "Virág", leiras: "Virágos kép kép"
+    eleres: "kepek/virag_2.jpeg", cim: "2Virág", leiras: "2. kép leírása"
 }, {
-    eleres: "kepek/virag_3.jpeg", cim: "Virág", leiras: "Virágos kép kép kép"
+    eleres: "kepek/virag_3.jpeg", cim: "3Virág", leiras: "3. kép leírása"
 }, {
-    eleres: "kepek/virag_4.jpeg", cim: "Virág", leiras: "Virágos kép kép kép kép"
+    eleres: "kepek/virag_4.jpeg", cim: "4Virág", leiras: "4. kép leírása"
 }];
